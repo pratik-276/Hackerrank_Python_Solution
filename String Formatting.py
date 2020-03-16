f4af850b65a3fae36b3a648c2ad4671e29ff2fd9
Pratik Mukherjee <pratikmukherjee32@gmail.com>
2020-03-16 21:25:14 +0530

    problem 23 updated

--- String Formatting.py
+++ String Formatting.py
@@ -1,7 +1,7 @@
 def print_formatted(number):
     w=len(bin(number)[2:])
     for i in range(1,number+1):
-        print(str(i).rjust(w,' '), str(oct(i)[2:]).rjust(w,' '), str(hex(i)[2:]).rjust(w,' '), str(bin(i)[2:]).rjust(w,' '))
+        print(str(i).rjust(w,' '), str(oct(i)[2:]).rjust(w,' '), str(hex(i)[2:]).rjust(w,' ').upper(), str(bin(i)[2:]).rjust(w,' '))
 
 
 if __name__ == '__main__':
